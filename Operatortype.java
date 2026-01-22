233efdff805e4e33a0083f3d65b2822b3013b509
cherry pick <limhojin956@naver.com>
2026-01-23 00:07:59 +0900

    feat : 제네릭 연산 기능 구현

--- Operatortype.java
+++ Operatortype.java
@@ -3,29 +3,31 @@ package Calculator8;
 
 public enum Operatortype {
 
+    //전부다 제네릭 타입으로 변환
+
     SUM('+') {
         @Override
-        public int calculate(int num1, int num2) {return num1 +num2;}
+        public <T extends Number> double calculate(T num1, T num2) {return num1.doubleValue() + num2.doubleValue();}
     }, // SUM 열거
 
     SUB('-') {
         @Override
-        public int calculate ( int num1, int num2){return num1 - num2;}
+        public <T extends Number> double calculate (T num1, T num2){return num1.doubleValue() - num2.doubleValue();}
     }, // SUB 열거
 
     MUL('*') {
         @Override
-        public int calculate ( int num1, int num2){return num1 * num2;}
+        public <T extends Number> double calculate (T num1, T num2){return num1.doubleValue() * num2.doubleValue();}
     }, // MUL 열거
 
 
     DIV('/') {
         @Override
-        public int calculate ( int num1, int num2){
-            if (num2 == 0) {
+        public <T extends Number> double calculate (T num1, T num2){
+            if (num2.doubleValue() == 0) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
             }
-            return num1 / num2;
+            return num1.doubleValue() / num2.doubleValue();
         }
     };  // DIV 열거
 
@@ -35,9 +37,9 @@ public enum Operatortype {
     Operatortype(char cal) {this.cal = cal;}
     // Enum 생성자 SUM, SUB, MUL, DIV 강제
 
-    public abstract int calculate(int num1, int num2);
+    public abstract <T extends Number> double calculate(T num1, T num2);
 
-    // 열거된 기능을 사용하기 위한 추상 메서드 구현
+    // 열거된 기능을 사용하기 위한 추상 메서드 구현 <- 연산을 여기서 하기에 제네릭 추상메서드 구현(반환타입 추가)
 
 
 }
