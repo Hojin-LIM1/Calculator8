de7ad82e9f041d9176d84500303467ebaccf2802
cherry pick <limhojin956@naver.com>
2026-01-22 22:58:50 +0900

    feat : 연산 enum class로 관리, docs : enum class관리에 따른 Calculator Class 미사용, 연산결과 저장 및 삭제 메소드는 ArithmeticCalculator로 구현

--- Operatortype.java
+++ Operatortype.java
@@ -1,38 +1,43 @@
+package Calculator8;
+
+
 public enum Operatortype {
+
     SUM('+') {
-        public double calculate(double num1 +double num2) {
-            return num1 +num2;
-        }
-    }
+        @Override
+        public int calculate(int num1, int num2) {return num1 +num2;}
+    }, // SUM 열거
 
     SUB('-') {
-        public double calculate ( double num1 - double num2){
-            return num1 - num2;
-        }
-    }
+        @Override
+        public int calculate ( int num1, int num2){return num1 - num2;}
+    }, // SUB 열거
 
     MUL('*') {
-        public double calculate ( double num1 *double num2){
-            return num1 * num2;
-        }
-    }
+        @Override
+        public int calculate ( int num1, int num2){return num1 * num2;}
+    }, // MUL 열거
 
 
     DIV('/') {
-        public double calculate ( double num1 /double num2){
-            if (num2 == 0){
+        @Override
+        public int calculate ( int num1, int num2){
+            if (num2 == 0) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
             }
             return num1 / num2;
-    }
+        }
+    };  // DIV 열거
 
     private final char cal;
+    // Operatetype 내에서만 쓸 수 있게 cal 상수화
 
-        Operatetype(char cal) {
-            this.cal = cal
-        }
+    Operatortype(char cal) {this.cal = cal;}
+    // Enum 생성자 SUM, SUB, MUL, DIV 강제
 
-        public abstract double calculate(double num1, double num2);
+    public abstract int calculate(int num1, int num2);
+
+    // 열거된 기능을 사용하기 위한 추상 메서드 구현
 
 
 }
